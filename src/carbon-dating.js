f4fe667cd63d30b686cb6ce571c16a1111d9f6b6
KotdenGit <kotden09@gmail.com>
2020-03-21 00:28:31 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,5 +3,12 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
   throw 'Not implemented';
-  // remove line with error and write your code here
+  let numsampleActivity = parseFloat(sampleActivity);
+  if(typeof(sampleActivity) !== 'string' || isNaN(numsampleActivity) || Number(numsampleActivity) <= 0 || Number(numsampleActivity) > MODERN_ACTIVITY) //Specific requirements
+  { 
+    return false;
+  } else {
+
+    return Math.ceil((Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity)) / k));
+  }// remove line with error and write your code here
 };
