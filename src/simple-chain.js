3ee0064c971302c8d5c60f0e57e4107d62ab09d4
KotdenGit <kotden09@gmail.com>
2020-03-21 00:44:01 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,33 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`)
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if ( position - 1 < 0 || !(typeof position === 'number') || this.chain.length < position  ) {
+      this.chain = [];
+      throw new Error();
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const chainStr = this.chain.join('~~');
+    this.chain = [];
+    return chainStr;
   }
 };
 
